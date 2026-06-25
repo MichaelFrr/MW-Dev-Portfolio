0a705f2bc9337f345e1e9c78afb6791f9091e179
Michael <yassenmana1@gmail.com>
2026-06-25 17:40:38 +0300

    Refactor bank_account class to include email and password

--- Python/Banking-App-FastAPI/banking_app_class.py
+++ Python/Banking-App-FastAPI/banking_app_class.py
@@ -2,70 +2,68 @@
 import json
 from pathlib import Path
 import datetime
-now = datetime.datetime.now()
 
 
 class bank_account:
 
-    def __init__(self, owner, balance, credit_score, id):
+    def __init__(self, owner, password, email, balance, credit_score, iban):
         self.owner = owner
+        self.password = password
+        self.email = email
         self.balance = balance
         self.credit_score = credit_score
-        self.transactions = []
-        self.id = id
+        self.iban = iban
+        # self.DOB = DOB
 
     def to_dict(self):
 
         return {
             "owner": self.owner,
+            "email": self.email,
             "balance": self.balance,
             "credit_score": self.credit_score,
-            "transactions": self.transactions
+            "iban": self.iban
+
         }
-
-    def withdraw(self, amount):
-        if self.balance - amount >= 0:
-            self.balance = self.balance - amount
-            self.transactions.append({"type": "withdraw", "amount": amount})
-            print(
-                f"You have successfuly withdrawed {amount} from your account you have {self.balance} left in your account.")
-        else:
-            print("You cannot Go in the negatives")
-
-    def deposit(self, amount):
-        self.balance = amount + self.balance
-        self.transactions.append({"type": "deposit", "amount": amount})
-        print(
-            f"You have successfuly deposited {amount} into your account you currently have {self.balance} left.")
 
     def check_balance(self):
         print(self.balance)
         return self.balance
 
+
+class account_finances:
+    def __init__(self, balance, action_type, transaction_id, account_id):
+        self.balance = balance
+        self.action_type = action_type
+        self.transaction_id = transaction_id
+        self.account_id = account_id
+        self.transactions = []
+
+    def action(self, action_type, amount):
+        now = datetime.datetime.now()
+        if action_type == "withdraw":
+            if self.balance - amount >= 0:
+                self.balance = self.balance - amount
+                self.transactions.append(
+                    {"type": "withdraw", "amount": amount, "timestamp": now})
+                print(
+                    f"You have successfuly withdrawed {amount} from your account you have {self.balance} left in your account.")
+            else:
+                print("You cannot go in the negatives")
+
+        elif action_type == "deposit":
+            self.balance = amount + self.balance
+            self.transactions.append({"type": "deposit", "amount": amount})
+            print(
+                f"You have successfuly deposited {amount} into your account you currently have {self.balance} left.")
+
+
     def show_transactions(self):
         print(self.transactions)
         return (self.transactions)
 
-    def save(self):
-        data = {
-            "owner": self.owner,
-            "balance": self.balance,
-            "credit_score": self.credit_score,
-            "transactions": self.transactions,
-            "Date": now.isoformat()
+    def to_dict(self):
+
+        return {
+            "transactions": self.transactions
         }
-
-        with open(f"{self.owner}_account.json", "w") as file:
-            json.dump(data, file, indent=4)
-
-    def load(self, owner):
-        filepath = Path(f"{self.owner}_account.json")
-        if not filepath.exists():
-            print(f"No saved data found for {self.owner}. Starting fresh.")
-            return
-        with open(f"{owner}_account.json", "r") as file:
-            data = json.load(file)
-        self.owner = data["owner"]
-        self.balance = data["balance"]
-        self.credit_score = data["credit_score"]
-        self.transactions = data["transactions"]
